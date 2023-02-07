25837bd53aa052734f978298f8e32f583b70279d
changiljeong <changil.jeong@tlgcohort.com>
2023-02-06 22:30:46 -0600

    added login button and complete the implementation of the feature with Auth0

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
+  <Auth0Provider
+  domain="dev-2tz8txuce56kews0.us.auth0.com"
+  clientId="ApDVcFcdvoyMLF5aH5KSvrcSA9t4lZcQ"
+  authorizationParams={{
+    redirect_uri: window.location.origin
+  }}>
+    
 <BrowserRouter>
      <App />
-  </BrowserRouter>
+  </BrowserRouter>, 
+</Auth0Provider>,
+
+
 );
 
 // If you want to start measuring performance in your app, pass a function
