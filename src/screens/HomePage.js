2c73fd589e504fc9022d85af2fa08259de6aa7cf
changiljeong <changil.jeong@tlgcohort.com>
2023-02-06 18:42:22 -0600

    added componenet, pages, and nav bars

---
+++ src/screens/HomePage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from "../components/Header"
+
+function HomePage() {
+    return (
+
+        <div>
+
+        <Header />
+            <h1>Welcome to Agora!</h1>
+
+        </div>
+    );
+}
+
+export default HomePage;
---
+++ src/screens/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from '../components/Header';
+import Form from '../components/Form';
+
+
+
+function Profile() {
+    return (
+      <div>
+        <Header />
+
+        <h1>User profile page</h1>
+
+        
+        <Form />
+
+
+
+
+
+        </div>
+    );
+  }
+  
+  export default Profile;
+  
