505111ec47ef33cc2a6a07ab0690e208f55c00f6
changiljeong <changil.jeong@tlgcohort.com>
2023-02-09 14:42:43 -0600

    removed Button hook

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import { Tabs, Tab } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from '@mui/material';
 import logo from '../assets/images/logo-black.png';
 import { BrowserRouter } from 'react-router-dom';
 
